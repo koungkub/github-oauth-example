622f6f7eb57b2d5f1aa7ee108e4eac0e28718286
koungkub <1997jirasak@gmail.com>
2019-08-07 16:29:43 +0700

    💽

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/koungkub/github-oauth-example
 go 1.12
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.1.8
+	github.com/labstack/gommon v0.2.9
 	github.com/spf13/viper v1.4.0
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 )
--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/random"
 	"github.com/spf13/viper"
-)
-
-var (
-	githubOAuth = oauth2.Config{
-		ClientID:     viper.GetString("ID"),
-		ClientSecret: viper.GetString("SECRET"),
-		RedirectURL:  viper.GetString("REDIRECTURL"),
-		Scopes:       []string{},
-		Endpoint:     github.Endpoint,
-	}
 )
 
 func init() {
@@ -27,5 +20,35 @@ func init() {
 }
 
 func main() {
-	fmt.Println(viper.GetString("ID"))
+
+	e := echo.New()
+
+	githubOAuth := oauth2.Config{
+		ClientID:     viper.GetString("ID"),
+		ClientSecret: viper.GetString("SECRET"),
+		RedirectURL:  viper.GetString("REDIRECTURL"),
+		Scopes:       []string{},
+		Endpoint:     github.Endpoint,
+	}
+
+	code := random.String(8)
+
+	e.GET("/", func(c echo.Context) error {
+		html := `
+			<a href="/oauth/github">login github here !!</a>	
+		`
+		return c.HTML(http.StatusOK, html)
+	})
+
+	e.GET("/oauth/github", func(c echo.Context) error {
+		// allowSignup := oauth2.SetAuthURLParam("allow_signup", "true")
+		url := githubOAuth.AuthCodeURL(code)
+		return c.Redirect(http.StatusTemporaryRedirect, url)
+	})
+
+	// e.GET("/oauth/callback", func(c echo.Context) error {
+	// 	return
+	// })
+
+	log.Fatal(e.Start(":1323"))
 }
